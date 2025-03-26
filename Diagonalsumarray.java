74b564950463462240c91f4aa5a990c7092c436f
yugender936 <yugenderenugala936@gmail.com>
2025-03-26 11:04:24 +0530

    Add files via upload

---
+++ Diagonalsumarray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Reversediagonalsumarray
+{
+	
+	public static void main(String[] args) 
+	{ 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array row length");
+		int n = sc.nextInt();
+		System.out.println("Enter array column length");
+		int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+		System.out.println("Enter array Values");
+		for(int i = 0 ; i < arr.length; i++){
+			for(int j = 0 ; j < arr[i].length ; j++){
+				arr[i][j] = sc.nextInt();
+			}
+
+		}
+		int sum = 0;
+		for(int i = 0 ; i < arr.length ; i++){
+			for(int j = 0 ; j < arr[i].length ; j++){
+				if(i+j == n-1){
+					sum = sum + arr[i][j];
+				}
+			}
+		}
+		System.out.println("sum of diagonals in the array is "+sum);
+
+
+
+		
+  }
+}
